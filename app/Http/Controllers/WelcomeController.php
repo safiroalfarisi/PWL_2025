c9842103f523784bed6358785e8ba97468827170
Safiro Alfarisi Haraya <sfralfarisi@gmail.com>
2025-02-19 11:33:18 +0700

    add: Praktikum3 jobsheet2

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class WelcomeController extends Controller
-{
-    public function hello() {
-        return 'Hello World';
+class WelcomeController extends Controller{
+    public function hello(){
+        return('Hello World');
+        }
+    public function greeting(){
+        return view('blog.hello')
+        ->with('name','Shiro')
+        ->with('occupation','Astronaut');
         }
 }
+
